0ecb4816f0a07dfbaf405027f987c49c494064cb
han1202012 <octopus_truth@163.com>
2020-07-09 22:33:41 +0800

    update

--- app/src/main/java/kim/hsl/aa/MyAsyncTask.java
+++ app/src/main/java/kim/hsl/aa/MyAsyncTask.java
@@ -3,12 +3,12 @@ package kim.hsl.aa;
 import android.os.AsyncTask;
 
 /**
- * AsyncTask<Void, Void, Void> 泛型解析
+ * AsyncTask<String, Integer, Boolean> 泛型解析
  * - 1. 异步任务开始时 , execute 方法传入的参数类型
  * - 2. 异步任务执行时 , 进度值类型
  * - 3. 异步任务结束时 , 结果类型
  */
-public class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+public class MyAsyncTask extends AsyncTask<String, Integer, Boolean> {
 
     @Override
     protected void onPreExecute() {
@@ -17,22 +17,22 @@ public class MyAsyncTask extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
+    protected Boolean doInBackground(String... strings) {
         // 主要的耗时操作是在该方法中执行的 ( 非主线程, 不能更新 UI )
         return null;
     }
 
     @Override
-    protected void onProgressUpdate(Void... values) {
+    protected void onProgressUpdate(Integer... values) {
         // 在 doInBackground 中调用了 publishProgress 方法, 就会回调该方法
         // 一般情况下是在该方法中执行更新 UI 的操作 ( 主线程, 可以更新 UI )
         super.onProgressUpdate(values);
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
+    protected void onPostExecute(Boolean aBoolean) {
         // doInBackground 执行完毕后 , 调用 return 方法后 , 该方法会被调用 ( 主线程, 可以更新 UI )
-        super.onPostExecute(aVoid);
+        super.onPostExecute(aBoolean);
     }
 
     @Override
@@ -41,7 +41,7 @@ public class MyAsyncTask extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-    protected void onCancelled(Void aVoid) {
-        super.onCancelled(aVoid);
+    protected void onCancelled(Boolean aBoolean) {
+        super.onCancelled(aBoolean);
     }
 }
